b8f39e9a9ee5d2ce27edca2c3bda5795377959cc
Placide IRANDORA <placideirandora@gmail.com>
2019-12-21 00:45:30 +0200

    Improve the homepage's UI

--- piano/src/App.vue
+++ piano/src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="app">
+    <img alt="Piano Logo" src="./assets/icons/keyboard.svg" class="app__logo">
+    <h1 class="app__title">Virtual Piano</h1>
   </div>
 </template>
 
@@ -10,13 +11,26 @@ export default {
 }
 </script>
 
-<style>
-#app {
+<style lang="scss">
+body {
+  background:  #E9F0F2;
+}
+
+.app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  &__logo {
+    width: 8rem;
+  }
+
+  &__title {
+    font-size: 3rem;
+    text-transform: uppercase;
+  }
 }
 </style>
