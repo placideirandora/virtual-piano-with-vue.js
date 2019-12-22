033c7e8dcb36d43d3ce4162d53589751ea0531b9
Placide IRANDORA <placideirandora@gmail.com>
2019-12-22 14:58:33 +0200

    Play the sound upon key press  and format the document with Vetur

--- piano/src/components/Piano.vue
+++ piano/src/components/Piano.vue
@@ -1,75 +1,85 @@
 <template>
-    <div class="wrapper">
-        <div class="wrapper__piano">
-            <div data-note="C" class="wrapper__key wrapper__key--white"></div>
-            <div data-note="Db" class="wrapper__key wrapper__key--black"></div>
-            <div data-note="D" class="wrapper__key wrapper__key--white"></div>
-            <div data-note="E" class="wrapper__key wrapper__key--black"></div>
-            <div data-note="Eb" class="wrapper__key wrapper__key--white"></div>
-            <div data-note="F" class="wrapper__key wrapper__key--white"></div>
-            <div data-note="Gb" class="wrapper__key wrapper__key--black"></div>
-            <div data-note="G" class="wrapper__key wrapper__key--white"></div>
-            <div data-note="Ab" class="wrapper__key wrapper__key--black"></div>
-            <div data-note="A" class="wrapper__key wrapper__key--white"></div>
-            <div data-note="Bb" class="wrapper__key wrapper__key--black"></div>
-            <div data-note="B" class="wrapper__key wrapper__key--white"></div>
-        </div>
-
-        <audio id="C" src="../assets/notes/C.mp3"></audio>
-        <audio id="Db" src="../assets/notes/Db.mp3"></audio>
-        <audio id="D" src="../assets/notes/D.mp3"></audio>
-        <audio id="E" src="../assets/notes/E.mp3"></audio>
-        <audio id="Eb" src="../assets/notes/Eb.mp3"></audio>
-        <audio id="F" src="../assets/notes/F.mp3"></audio>
-        <audio id="Gb" src="../assets/notes/Gb.mp3"></audio>
-        <audio id="G" src="../assets/notes/G.mp3"></audio>
-        <audio id="Ab" src="../assets/notes/Ab.mp3"></audio>
-        <audio id="A" src="../assets/notes/A.mp3"></audio>
-        <audio id="Bb" src="../assets/notes/Bb.mp3"></audio>
-        <audio id="B" src="../assets/notes/B.mp3"></audio>
+  <div class="wrapper">
+    <div class="wrapper__piano">
+      <div class="wrapper__key wrapper__key--white" @click="playNote('#C')"></div>
+      <div class="wrapper__key wrapper__key--black" @click="playNote('#Db')"></div>
+      <div class="wrapper__key wrapper__key--white" @click="playNote('#D')"></div>
+      <div class="wrapper__key wrapper__key--black" @click="playNote('#E')"></div>
+      <div class="wrapper__key wrapper__key--white" @click="playNote('#Eb')"></div>
+      <div class="wrapper__key wrapper__key--white" @click="playNote('#F')"></div>
+      <div class="wrapper__key wrapper__key--black" @click="playNote('#Gb')"></div>
+      <div class="wrapper__key wrapper__key--white" @click="playNote('#G')"></div>
+      <div class="wrapper__key wrapper__key--black" @click="playNote('#Ab')"></div>
+      <div class="wrapper__key wrapper__key--white" @click="playNote('#A')"></div>
+      <div class="wrapper__key wrapper__key--black" @click="playNote('#Bb')"></div>
+      <div class="wrapper__key wrapper__key--white" @click="playNote('#B')"></div>
     </div>
+
+    <audio id="C" src="../assets/notes/C.mp3"></audio>
+    <audio id="Db" src="../assets/notes/Db.mp3"></audio>
+    <audio id="D" src="../assets/notes/D.mp3"></audio>
+    <audio id="E" src="../assets/notes/E.mp3"></audio>
+    <audio id="Eb" src="../assets/notes/Eb.mp3"></audio>
+    <audio id="F" src="../assets/notes/F.mp3"></audio>
+    <audio id="Gb" src="../assets/notes/Gb.mp3"></audio>
+    <audio id="G" src="../assets/notes/G.mp3"></audio>
+    <audio id="Ab" src="../assets/notes/Ab.mp3"></audio>
+    <audio id="A" src="../assets/notes/A.mp3"></audio>
+    <audio id="Bb" src="../assets/notes/Bb.mp3"></audio>
+    <audio id="B" src="../assets/notes/B.mp3"></audio>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Piano'
-}
+  name: "Piano",
+  methods: {
+    playNote(key) {
+      const noteAudio = document.querySelector(key);
+
+      noteAudio.currentTime = 0;
+      noteAudio.play();
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    *, *::before, *::after {
-        box-sizing: border-box;
-    }
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
 
-    .wrapper {
-        background: #41B883;
-        margin: 0;
-        min-height: 50vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: #E9F0F2;
+.wrapper {
+  background: #41b883;
+  margin: 0;
+  min-height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #e9f0f2;
 
-        &__piano {
-            display: flex; 
-        }
+  &__piano {
+    display: flex;
+  }
 
-        &__key {
-            height: calc(var(--width) * 4);
-            width: var(--width);
-        }
+  &__key {
+    height: calc(var(--width) * 4);
+    width: var(--width);
+  }
 
-        &__key--white {
-            --width: 100px;
-            background: #E9F0F2;
-            border: 1px solid #2c3e50;
-        }
+  &__key--white {
+    --width: 100px;
+    background: #e9f0f2;
+    border: 1px solid #2c3e50;
+  }
 
-        &__key--black {
-            --width: 60px;
-            background: #000;
-            margin: 0 calc(var(--width) / -2);
-            z-index: 2;
-        }
-    }
+  &__key--black {
+    --width: 60px;
+    background: #000;
+    margin: 0 calc(var(--width) / -2);
+    z-index: 2;
+  }
+}
 </style>
